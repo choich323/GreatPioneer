bd872196224bfad24dd0b6de881aff5bcf941512
choich <choich323@khu.ac.kr>
2026-03-05 01:01:19 +0900

    프리팹 ID를 스트링으로 기입하게 수정

--- Assets/Scripts/Data/APrefabData.cs
+++ Assets/Scripts/Data/APrefabData.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [Serializable]
 public struct PrefabInfo
 {
-    public PrefabID id;
+    public string prefabId;
     public GameObject prefab;
 }
 
--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -19,7 +20,7 @@ public class DataManager : MonoBehaviour
             if (data == null) continue;
             foreach (var info in data.prefabInfoList)
             {
-                var key = (int)info.id;
+                var key = (int)ConvertStringToPrefabID(info.prefabId);
                 if (key == (int)PrefabID.None) continue;
                 
                 if (!_prefabMap.ContainsKey(key))
@@ -30,6 +31,15 @@ public class DataManager : MonoBehaviour
         }
     }
 
+    PrefabID ConvertStringToPrefabID(string argPrefabId)
+    {
+        if (Enum.TryParse(argPrefabId, true, out PrefabID prefabId))
+        {
+            return prefabId;
+        }
+        return PrefabID.None;
+    }
+    
     public bool TryGetPrefab(int argId, out GameObject outObject)
     {
         return _prefabMap.TryGetValue(argId, out outObject);
